ed9789ca7a4547192798b4f084552049f9d64b7f
JoaoSuriano17 <joao.suriano@aluno.ifsp.edu.br>
2026-08-27 13:16:04 -0300

    Tarefa Finalizada - Criar insert geral e específico na rota editora

--- src/routes/editora.js
+++ src/routes/editora.js
@@ -2,8 +2,27 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ msg: "Bem vindo ao express_template_docker!!!" });
+//Get - geral
+router.get("/", async (req, res) => {
+  const r = await db.query("SELECT * FROM editora")
+  if (r.rowCount == 0){
+    return res.status(404).json({msg: "Não há nenhum autor!"})
+  }
+
+  res.status(200).json(r.rows)
 });
+
+//GEt - específico
+router.get("/:id", async (req, res) => {
+  let id = req.params.id
+
+  const r = await db.query("SELECT * FROM editora WHERE id = $1", [id])
+  if (r.rowCount == 0){
+    return res.status(404).json({msg: "Esta editora não existe!"})
+  }
+
+  res.status(200).json(r.rows)
+});
+
 
 module.exports = router;
