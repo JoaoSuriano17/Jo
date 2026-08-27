5dbdb1db67f4adcf53e6253381eefd22b61c28bd
JoaoSuriano17 <joao.suriano@aluno.ifsp.edu.br>
2026-08-27 13:12:20 -0300

    Tarefa Finalizada - Criar insert geral e específico na rota autor

--- src/routes/autor.js
+++ src/routes/autor.js
@@ -2,8 +2,26 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ msg: "Bem vindo ao express_template_docker!!!" });
+//Get - geral
+router.get("/", async (req, res) => {
+  const r = await db.query("SELECT * FROM autor")
+  if (r.rowCount == 0){
+    return res.status(404).json({msg: "Não há nenhum autor!"})
+  }
+
+  res.status(200).json(r.rows)
+});
+
+//GEt - específico
+router.get("/:id", async (req, res) => {
+  let id = req.params.id
+
+  const r = await db.query("SELECT * FROM autor WHERE id = $1", [id])
+  if (r.rowCount == 0){
+    return res.status(404).json({msg: "Este autor não existe!"})
+  }
+
+  res.status(200).json(r.rows)
 });
 
 module.exports = router;
